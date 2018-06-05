4dd1b0f50af97bc3f5f314712dc4d9a8337cdb63
benmcmurry <benmcmurry@gmail.com>
2018-06-04 22:30:35 -0600

    minor fix

--- index.php
+++ index.php
@@ -69,12 +69,7 @@ $result = $result->fetch_assoc();
         <div id="header">
             <?php include_once("common_content/header.php");?>
         </div>
-        <?php
-           foreach($_POST as $key => $value) {
-            print "$key=$value\n";
-        }
-
-?>
+        
         <?php
         if ($instructor !== FALSE) {
             include_once('teacherView.php');
